7048f1faa1fb214e1651efa975a2d8080cbebccb
betawen <jing_find@163.com>
2019-08-15 15:48:30 +0800

    牛客网刷题

---
+++ nowCoder/Permutation.js
@@ -0,0 +1,14 @@
+function Permutation(str) {
+    let arr = str.split('');
+
+    console.log(arr);
+}
+function swap(arr,res,tmp){
+    let len = arr.length;
+    let i, j;
+    while (i < len){
+        tmp.l
+    }
+}
+
+Permutation('abc')
