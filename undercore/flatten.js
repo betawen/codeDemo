f02093a3b8fd88d1926ea7e560b04c206b9e4f8e
betawen <jing_find@163.com>
2019-09-26 10:50:44 +0800

    flatten check

---
+++ undercore/flatten.js
@@ -0,0 +1,20 @@
+// 方法 1
+// var arr = [1, [2, [3, 4]]];
+
+function flatten(arr) {
+    var result = [];
+    for (var i = 0, len = arr.length; i < len; i++) {
+        if (Array.isArray(arr[i])) {
+            result = result.concat(flatten(arr[i]))
+        }
+        else {
+            result.push(arr[i])
+        }
+    }
+    return result;
+}
+
+
+// console.log(flatten(arr))
+
+console.log(flatten([1,2,3,[1,2,[3]]]))
